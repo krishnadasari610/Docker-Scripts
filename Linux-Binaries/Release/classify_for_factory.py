01ee5ebc27b2e4203ccad50763c5b6d66d4f9264
krishnadasari610 <krishnadasari610@gmail.com>
2019-06-07 10:25:12 +0530

    added new script files

--- Linux-Binaries/Release/classify_for_factory.py
+++
@@ -1,4 +0,0 @@
-def classify(model_file, num_classes, bytes, bits_allocated = 16):
-	print ">> Example classification on an array with ", num_classes, " classes" 
-	print ">> Apply model_file ", model_file
-	
